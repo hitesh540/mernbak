5a2e20e3678ba2b346de1bcf15c727c555c39195
hitesh borse <nskhitesh2016@gmail.com>
2021-03-16 16:55:50 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+app.use(cookieParser());
+app.use(express.json());
+require('dotenv/config');
+
+
+const userRouter = require('./routes/User');
+app.use('/user',userRouter);
+
+
+
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+
+// mongoose.connect( process.env.CONNECTION_STRING,{
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//     useCreateIndex: true
+// },
+// ()=>{
+//     console.log('successfully connected to database');
+// });
+
+
+
+//Database
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
+.then(() => {
+    console.log('Database connection is ready..');
+})
+.catch((err) => {
+    console.log(err);
+})
+
+
+//Server
+app.listen(5000,()=>{
+    console.log('express server started');
+});
